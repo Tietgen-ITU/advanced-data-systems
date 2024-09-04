32cf87d4abba8c70d2a19e5302640770dd0804bc
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2024-09-04 18:05:50 +0200

    Calculate the probability for each word to appear in the category

--- exercise-01/scripts/naive_bayes.sql
+++ exercise-01/scripts/naive_bayes.sql
@@ -2,3 +2,12 @@
 select label, sum(count) as words 
 from category_word_count
 group by label;
+
+-- Calculate the propability of word in category
+set category = 0;
+
+select cwc.*, t.words, (cwc.count/t.words) as prop
+from category_word_count as cwc
+inner join (select label, sum(count) as words 
+     from category_word_count
+     group by label) as t on t.label = cwc.label;
