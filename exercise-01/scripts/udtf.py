376d9fc4f3188e9dfe774356f678cce2c350421e
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2024-09-14 11:51:07 +0200

    Add python file with basic functions to handle UDTF spec from snowflake

---
+++ exercise-01/scripts/udtf.py
@@ -0,0 +1,13 @@
+class CheetahUDTF:
+    def __init__(self):
+        
+
+    def process(self, is_training, label, text):
+        
+
+    def end_partition(self):
+        
+
+
+if __name__ == '__main__':
+    
