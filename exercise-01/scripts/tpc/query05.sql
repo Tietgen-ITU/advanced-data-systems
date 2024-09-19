768f51192bd0cc22d21f6637571c365d798435ad
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2024-09-19 13:31:07 +0200

    Add query 5 with parameters defined

--- exercise-01/scripts/tpc/query05.sql
+++ exercise-01/scripts/tpc/query05.sql
@@ -1,4 +1,8 @@
 ALTER SESSION SET USE_CACHED_RESULT = FALSE;
+
+set region = 'ASIA';
+set query_date = '1997-01-01'
+
 select
     n_name,
     sum(l_extendedprice * (1 - l_discount)) as revenue
@@ -16,8 +20,8 @@ where
     and c_nationkey = s_nationkey
     and s_nationkey = n_nationkey
     and n_regionkey = r_regionkey
-    and r_name = '[REGION]'
-    and o_orderdate >= date '[DATE]'
-    and o_orderdate < date '[DATE]' + interval '1' year
+    and r_name = $region
+    and o_orderdate >= $query_date
+    and o_orderdate < DATEADD(YEAR, 1, $query_date)
 group by n_name
 order by revenue desc;
